2d528c3d6dc1464299b3d673aa68fc8c9aef69b7
billjirik <william.jirik@ndsu.edu>
2024-04-03 20:11:47 -0500

    authentication

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -36,4 +36,7 @@ urlpatterns += [
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
 
